23918d9bef97d08f41575e7c27177f561f4ad011
Christian E <christianelena92@gmail.com>
2022-04-28 12:18:45 -0500

    using Destructuring vs props

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import Header from "./Header";
 import "./App.css";
 
 function App() {
+  const techItems = ["macBook", "ipods", "speakerbox"];
+
   return (
     <div className="App">
-      <Header />
+      <Header tech={techItems} bob={["items", "items2"]} />
     </div>
   );
 }
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./Header.css";
 
-const Header = (props) => {
+const Header = ({ tech, bob }) => {
   return (
     <div className="App-header">
       <h1>Amazon Cart</h1>
+      <h2>{tech[0]}</h2>
+      <p> {bob[1]}</p>
     </div>
   );
 };
