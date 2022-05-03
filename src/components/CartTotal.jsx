7c437e1077aeef07310bdc9c86dbf400290e784d
Christian E <christianelena92@gmail.com>
2022-05-03 02:53:49 -0500

    CHNG: Format total price and style carTotal price componenet

--- src/components/CartTotal.jsx
+++ src/components/CartTotal.jsx
@@ -1,23 +1,32 @@
 import React from "react";
 import "./CartTotal.css";
+import NumberFormat from "react-number-format";
 
 const CartTotal = ({ items }) => {
   const getTotalPrice = () => {
-    // init total price to 0
     let total = 0;
-    // loop thorugh all the items in the cart
+
     items.forEach((item) => {
       total += item.price * item.quantity;
     });
-    // add the price of the item with the quantitiy to the total price
+
     return total;
-    // return the total price
   };
+  const totalItems = items.reduce((acc, item) => acc + item.quantity, 0);
+
   return (
     <div className="CartTotal">
       <h3>
-        Subtotal({items.length} items):
-        <span className="cartTotal-price">${getTotalPrice()}</span>
+        Subtotal({totalItems} items):
+        <div className="cartTotal-price">
+          <NumberFormat
+            value={getTotalPrice()}
+            displayType={"text"}
+            thousandSeparator={true}
+            prefix={"$"}
+            decimalScale={2}
+          />
+        </div>
       </h3>
       <button>Proceed to Checkout</button>
     </div>
