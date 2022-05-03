94ccd51f282adef5663a47929abb169c2b6a81af
Christian E <christianelena92@gmail.com>
2022-05-02 22:55:45 -0500

    Create HTML adn style for CartTotal Component and Button

--- src/components/CartTotal.jsx
+++ src/components/CartTotal.jsx
@@ -2,7 +2,14 @@ import React from "react";
 import "./CartTotal.css";
 
 const CartTotal = () => {
-  return <div className="CartTotal"></div>;
+  return (
+    <div className="CartTotal">
+      <h3>
+        Subtotal(5 items): <span className="cartTotal-price">$2172.38</span>
+      </h3>
+      <button>Proceed to Checkout</button>
+    </div>
+  );
 };
 
 export default CartTotal;
