c3e1d5b5ce49b51af84bea644637464ef9e91391
Christian E <christianelena92@gmail.com>
2022-05-21 05:05:31 -0500

    intall react-number-format package for formatting total price

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import "./CartItem.css";
 
-function CartItem({ item }) {
-  console.log(item.image);
+function CartItem({ index, item, changeItemQuantity }) {
   return (
     <div className="CartItem">
       <div className="CartItem-image">
         <img src={process.env.PUBLIC_URL + "./items/" + item.image} alt="" />
-        {/* <img src={process.env.PUBLIC_URL + "/yourPathHere.jpg"} /> */}
       </div>
       <div className="CartItem-info">
         <div className="Info-title">
@@ -16,11 +14,16 @@ function CartItem({ item }) {
         <div className="Info-stock">{item.stock}</div>
         <div className="Item-actions">
           <div className="Item-quantity">
-            <select value={item.quantity}>
+            <select
+              onChange={(e) => changeItemQuantity(e, index)}
+              value={item.quantity}
+            >
               <option value="1">Qty: 1</option>
               <option value="2">Qty: 2</option>
               <option value="3">Qty: 3</option>
               <option value="4">Qty: 4</option>
+              <option value="5">Qty: 5</option>
+              <option value="6">Qty: 6</option>
             </select>
           </div>
           <div className="items-actions-divider">|</div>
--- src/components/CartItems.jsx
+++ src/components/CartItems.jsx
@@ -2,14 +2,33 @@ import React from "react";
 import CartItem from "./CartItem";
 import "./CartItems.css";
 
-function CartItems({ items }) {
+function CartItems({ items, setCartItems }) {
+  const changeItemQuantity = (e, index) => {
+    // When we select a quanity on item, we pass it in here
+    // pass in the index
+    // using the index we need to change the quanity to the selected on form select option
+    // then we need to update the items state, because the state is inside our useState;
+    alert(e.target.value);
+    console.log("index", index);
+
+    // DO NOT UPDATE THE STATE WIHTOUT A SETTER FUNCTION IN REACT
+    // this will create a deep clone, COnstruct and assing to an array that is a deep copy clone
+    const newItems = [...items];
+    items[index].quantity = e.target.value;
+    setCartItems(items);
+  };
   return (
     <div className="CartItems">
       <h1>Amazon </h1>
       <hr />
       <div className="CartItems-items">
-        {items.map((item, idx) => (
-          <CartItem item={item} key={idx} />
+        {items.map((item, indx) => (
+          <CartItem
+            item={item}
+            key={indx}
+            index={indx}
+            changeItemQuantity={changeItemQuantity}
+          />
         ))}
       </div>
     </div>
